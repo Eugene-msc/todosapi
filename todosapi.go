04b1a2b069b95a8550d20d8489763f8753438612
Eugene <yeti.moscow@gmail.com>
2016-04-18 18:20:33 +0300

    added todosapi.go

---
+++ todosapi.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func helloHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`<doctype html>
+<html>
+	<head>
+		<title>Hello Go</title>
+	</head>
+	<body>
+	</body>
+</html>`,
+	)
+}
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go web app powered by Docker")
+}
+func main() {
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/hello", helloHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+		return
+	}
+}
